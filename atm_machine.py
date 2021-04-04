286f317b919acf95905f7e346f501f11f6a75b7a
Bestoonit <57560613+Bestoonit@users.noreply.github.com>
2021-04-04 20:23:01 +0200

    Add files via upload

---
+++ atm_machine.py
@@ -0,0 +1,48 @@
+class Atm:
+    def __init__(self, withdrawal, insert, balance):
+        self.withdrawal = withdrawal
+        self.insert = insert
+        self.balance = balance
+       
+    def balance_(self):
+        print('Your balance now is: {}'.format(self.balance))
+       
+    def insert_(self):
+        insert = int(input("How much money you want to insert: "))
+        self.balance = insert + self.balance
+        print('Your balance now is: {}'.format(self.balance))
+       
+    def withdrawal_(self):
+        withdrawal = int(input("How much money you want to take out: "))
+        self.balance = self.balance - withdrawal
+        print('Your balance now is: {}'.format(self.balance))
+   
+atm = Atm(0, 0, 0)
+
+def main():
+
+    while True:
+        print("\nSelect an option: \n")
+        print("1- Show balance")
+        print("2- Insert money")
+        print("3- Take out money")
+        print("4- Cancel ")
+        try:
+            val = int(input(": "))
+            if val == 1:
+                atm.balance_()
+            elif val == 2:
+                atm.insert_()
+            elif val == 3:
+                atm.withdrawal_()
+            elif val == 4:
+                break
+            elif val != 1 or val != 2 or val != 3 or val != 4:
+                print("Type one number between 1, 2, 3, 4 as an option. ")
+                main()
+        except ValueError:
+            print("Type one number between 1, 2, 3, 4 as an option. ")
+            main()
+
+if __name__ == "__main__":
+    main()
