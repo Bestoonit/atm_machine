8e878ec642deb151599af1bc16f9cdb149e868e2
bestoonit <bestoonhero@gmail.com>
2021-04-05 19:09:34 +0200

    last modified

--- atm_machine.py
+++ atm_machine.py
@@ -3,24 +3,24 @@ class Atm:
         self.withdrawal = withdrawal
         self.insert = insert
         self.balance = balance
-       
+
     def balance_(self):
         print('Your balance now is: {}'.format(self.balance))
-       
+
     def insert_(self):
         insert = int(input("How much money you want to insert: "))
         self.balance = insert + self.balance
         print('Your balance now is: {}'.format(self.balance))
-       
+
     def withdrawal_(self):
         withdrawal = int(input("How much money you want to take out: "))
         self.balance = self.balance - withdrawal
         print('Your balance now is: {}'.format(self.balance))
-   
+
 atm = Atm(0, 0, 0)
 
 def main():
-
+    choice = [1, 2, 3, 4]
     while True:
         print("\nSelect an option: \n")
         print("1- Show balance")
@@ -37,12 +37,14 @@ def main():
                 atm.withdrawal_()
             elif val == 4:
                 break
-            elif val != 1 or val != 2 or val != 3 or val != 4:
+            elif val not in choice:
                 print("Choose a number between 1, 2, 3, 4 as an option. ")
                 main()
+
         except ValueError:
             print("Choose a number between 1, 2, 3, 4 as an option. ")
             main()
 
 if __name__ == "__main__":
     main()
+
